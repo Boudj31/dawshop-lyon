23401ce92b0d449fb91a5074d328932205ed013b
Boudjenane Rachid <rboudjenane@jehann.fr>
2023-02-07 17:08:03 +0100

    starter projet :rocket::rocket:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='bg-cyan-500 text-center'>Hello word</h1>     
+    </div>
+  )
+}
+
+export default App
